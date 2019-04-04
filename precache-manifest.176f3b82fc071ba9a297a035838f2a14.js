c7aba4342b48974844c6e4b6355869ff0af7ec04
iinourah96@gmail.com <iinourah96@gmail.com>
2019-04-04 09:43:36 +0300

    Updates

---
+++ precache-manifest.176f3b82fc071ba9a297a035838f2a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa2b69c5e70a89dc44619589cb758fa",
+    "url": "/Food_Recipes/static/media/logo4.7aa2b69c.png"
+  },
+  {
+    "revision": "805aeb6fd09fc8b0f372",
+    "url": "/Food_Recipes/static/js/runtime~main.b96341e4.js"
+  },
+  {
+    "revision": "0c704d940717cc3161a6",
+    "url": "/Food_Recipes/static/js/main.aa77d6d6.chunk.js"
+  },
+  {
+    "revision": "19878e43076d93dce3e4",
+    "url": "/Food_Recipes/static/js/2.b910c674.chunk.js"
+  },
+  {
+    "revision": "0c704d940717cc3161a6",
+    "url": "/Food_Recipes/static/css/main.3414601b.chunk.css"
+  },
+  {
+    "revision": "5b9a7d782e8053d9350d3a9f2ab08f38",
+    "url": "/Food_Recipes/index.html"
+  }
+];
